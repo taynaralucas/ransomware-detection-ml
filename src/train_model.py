099fb5555710e9985092820fec800e931385e2a3
Taynara Lucas <taynaralucas2005@gmail.com>
2026-01-07 10:38:50 -0300

    Adiciona análise inicial e distribuição das classes do dataset

--- src/train_model.py
+++ src/train_model.py
@@ -1,19 +1,21 @@
 import pandas as pd
 
-# Caminho do dataset
 DATASET_PATH = "../data/malmem2022.csv"
 
 def load_dataset(path):
-    """
-    Carrega o dataset a partir de um arquivo CSV.
-    """
     df = pd.read_csv(path)
     return df
 
+def analyze_dataset(df):
+    print("Informações gerais do dataset:\n")
+    print(df.info())
+
+    print("\nDistribuição das classes:\n")
+    print(df["Class"].value_counts())
+
+    print("\nPercentual das classes:\n")
+    print(df["Class"].value_counts(normalize=True) * 100)
+
 if __name__ == "__main__":
     df = load_dataset(DATASET_PATH)
-
-    print("Dataset carregado com sucesso!")
-    print("Formato do dataset:", df.shape)
-    print("\nPrimeiras linhas:")
-    print(df.head())
+    analyze_dataset(df)
