7a0d6406d3aa80a6284abd1115e33ff1c6d96e74
Taynara Lucas <taynaralucas2005@gmail.com>
2026-01-08 09:25:20 -0300

    Remove parâmetro obsoleto do XGBoost

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+from xgboost import XGBClassifier
 
 DATASET_PATH = "../data/malmem2022.csv"
 
@@ -7,24 +10,40 @@ def load_dataset(path):
     return pd.read_csv(path)
 
 def prepare_data(df):
-    # Variável alvo
-    y = df["Class"]
+    # Codificando a variável alvo
+    le = LabelEncoder()
+    y = le.fit_transform(df["Class"])
 
-    # Removendo colunas não utilizadas no modelo
+    # Features
     X = df.drop(columns=["Class", "Category", "Filename"])
 
-    return X, y
+    return X, y, le
+
+
+def train_xgboost(X_train, y_train):
+    model = XGBClassifier(
+        n_estimators=100,
+        max_depth=6,
+        learning_rate=0.1,
+        random_state=42,
+        eval_metric="logloss",
+        
+    )
+    model.fit(X_train, y_train)
+    return model
 
 if __name__ == "__main__":
     df = load_dataset(DATASET_PATH)
-    X, y = prepare_data(df)
-
-    print("Formato de X:", X.shape)
-    print("Formato de y:", y.shape)
+    X, y, label_encoder = prepare_data(df)
 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
-    print("\nConjunto de treino:", X_train.shape)
-    print("Conjunto de teste:", X_test.shape)
+    model = train_xgboost(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    print("\nAcurácia:", accuracy_score(y_test, y_pred))
+    print("\nRelatório de classificação:\n")
+    print(classification_report(y_test, y_pred))
